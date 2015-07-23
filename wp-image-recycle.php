48a26e0bf1c85802ce9ab3722ed550ae009cafcc
ImageRecycle <ImageRecycle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-23 12:32:24 +0000

    description update
    git-svn-id: https://plugins.svn.wordpress.org/imagerecycle-pdf-image-compression/trunk@1204783 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-image-recycle.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: ImageRecycle pdf & image compression
+Plugin URI: https://www.imagerecycle.com/cms/wordpress
+Description: ImageRecycle is an automatic image & PDF optimizer that save up to 80% of the media weight without loosing quality. Speed up your website, keep visitors on board!
+Author: ImageRecycle
+Version: 1.0.3
+Author URI: https://www.imagerecycle.com
+Licence : GNU General Public License version 2 or later; http://www.gnu.org/licenses/gpl-2.0.html
+Copyright : Copyright (C) 2014 Imagerecycle (https://www.imagerecycle.com). All rights reserved.
+*/
+
+// Prohibit direct script loading
+defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
+
+if (!defined('WPIO_IMAGERECYCLE'))
+    define('WPIO_IMAGERECYCLE', plugin_dir_path(__FILE__));
+
+ define('WPIO_IMAGERECYCLE_VERSION', '1.0.3');
+require_once( WPIO_IMAGERECYCLE . 'class/class-image-otimizer.php' );
+register_activation_hook( __FILE__, array('wpImageRecycle','install') );
+register_uninstall_hook( __FILE__, array('wpImageRecycle','uninstall') );
+
+new wpImageRecycle();
